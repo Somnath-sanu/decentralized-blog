6929b6863b6ae1d3660714ef64824b1c4c2a03bb
Somnath Mishra <somnathmishra100dbi@gmail.com>
2025-11-11 21:08:05 +0530

    fix_typo

--- anchor/src/counter-exports.ts
+++ anchor/src/counter-exports.ts
@@ -1,8 +1,7 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
 import { AnchorProvider, Program } from '@coral-xyz/anchor'
 import { Cluster, PublicKey } from '@solana/web3.js'
-import CounterIDL from '../target/idl/curd.json'
-import type { Curd } from '../target/types/curd'
+import CounterIDL from '../target/idl/counter.json'
 import type { Counter } from '../target/types/counter'
 
 // Re-export the generated IDL and type
@@ -13,7 +12,7 @@ export const COUNTER_PROGRAM_ID = new PublicKey(CounterIDL.address)
 
 // This is a helper function to get the Counter Anchor program.
 export function getCounterProgram(provider: AnchorProvider, address?: PublicKey): Program<Counter> {
-  return new Program({ ...CounterIDL, address: address ? address.toBase58() : CounterIDL.address } as Curd, provider)
+  return new Program({ ...CounterIDL, address: address ? address.toBase58() : CounterIDL.address } as Counter, provider)
 }
 
 // This is a helper function to get the program ID for the Counter program depending on the cluster.
@@ -21,7 +20,6 @@ export function getCounterProgramId(cluster: Cluster) {
   switch (cluster) {
     case 'devnet':
     case 'testnet':
-    case 'mainnet-beta': //TODO: Remove this 
       // This is the program ID for the Counter program on devnet and testnet.
       return new PublicKey('7VnZZbZcQiaKeAYf3KGFDMTpbG5dyHHVhhXCSj9GNGGH')
     case 'mainnet-beta':
